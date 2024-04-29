77b5a2c2b822ff3ef6b8af5ced99d3b1129987c1
Alison <alison.peard@gmail.com>
2024-04-29 16:50:05 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -11,7 +11,8 @@ setup(
 
     packages=find_packages(),
     install_requires=[
-        "tensorflow-macos",
+        #"tensorflow-macos",
+        "tensorflow",
         "tensorflow-metal",
         "scipy",
         "pandas",
