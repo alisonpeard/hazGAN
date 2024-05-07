89c1a9781d27812be86ba0db1505395a032b678b
Alison Peard <alison.peard@gmail.com>
2024-05-07 18:22:14 +0100

    tidied up scripts

--- scripts/main.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/zsh
-
-source conda activate general
-python "1a_process_era5.py"
-R -f "1b_fit_marginals_era5.R"
-python "1c_make_training_era5.py"
-source activate tf_geo
-python "train_wgan.py"
