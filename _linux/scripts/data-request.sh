c07eeef26d13705643bba98f2aa76d43d33ee2ac
Alison Peard <alison.peard@gmail.com>
2024-05-17 14:58:56 +0100

    set up job array for era5

--- _linux/scripts/data-request.sh
+++ _linux/scripts/data-request.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
-which python
-python era5_api_request.py
+#SBATCH --job-name=era5_request
+#SBATCH --output=era5_request_%A_%a.out
+#SBATCH --array=1-876
+#SBATCH --time=01:00:00
+
+python era5_api_request.py $SLURM_ARRAY_TASK_ID
--- _linux/scripts/job-array.sh
+++ _linux/scripts/job-array.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #SBATCH --job-name=arrayTest
-#SBATCH --output=arrayTest_%A_%a.out
+#SBATCH --output=array_%A_%a.out
 #SBATCH --array=1-4
 #SBATCH --time=00:05:00
 
