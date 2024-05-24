5ad54ed0e6564e02ace247e6ed230badcb2ca405
Alison Peard <alison.peard@gmail.com>
2024-05-24 11:06:51 +0100

    small mod

--- scripts/data_processing/b_process_raw_era5.py
+++ scripts/data_processing/b_process_raw_era5.py
@@ -1,11 +1,16 @@
 """
-Environment: general
+Environment: hazGAN
 
 Process ERA5 data daily maximum wind speed at 10m and minimum MSLP.
 
-References:
------------
-    https://carpentries-lab.github.io/python-aos-lesson/10-large-data/index.html
+Input:
+------
+    - raw ERA5 data (netcdf) of form <datadir>/era5/new_data/*bangladesh*.nc
+
+Output:
+-------
+    - netcdf file of processed data (max wind speed, min MSLP) in <outdir>/era5_data/data_{year0}_{yearn}.nc
+    - parquet file of processed data in outdir/era5_data/data_{year0}_{yearn}.parquet
 """
 
 # %%
@@ -20,10 +25,10 @@ import pandas as pd
 import geopandas as gpd
 import matplotlib.pyplot as plt
 
-
+#  read direct from linux server
 wd = "/Users/alison/Documents/DPhil/multivariate"
-datadir = "/Users/alison/Documents/DPhil/data"
-indir = os.path.join(datadir, "era5", "new_data")
+indir = os.path.join('/Users', 'alison', 'Documents', 'DPhil', 'data', 'era5', 'new_data')
+# indir = os.path.join('/Volumes/spet5107/data/new_data/era5/')
 outdir = os.path.join(wd, "era5_data")
 
 # load all files and process to geopandas
@@ -33,31 +38,9 @@ ds["u10"] = np.sqrt(ds["u10"] ** 2 + ds["v10"] ** 2)
 ds = ds.drop_vars(["v10"])
 ds_full = ds.copy()
 
+# grab max/min values for later checks
 wind_max = ds_full.u10.values.max()
 pressure_min = ds_full.msl.values.min()
-# %%
-def output_size(h_in, k, s, p=0):
-    h_out = np.floor((h_in + 2 * p - k) / s) + 1
-    return int(h_out)
-
-# %%
-def find_kernel_stride_pairs(H_in, H_out):
-    pairs = []
-    for stride in range(1, H_in):
-        kernel_size_numerator = H_in - (H_out - 1) * stride
-        if kernel_size_numerator > 0 and kernel_size_numerator <= H_in:
-            for remainder in range(stride):
-                kernel_size = kernel_size_numerator + remainder
-                if kernel_size <= H_in:
-                    pairs.append((kernel_size, stride))
-    return pairs
-
-# Example usage
-H_in = 10
-H_out = 5
-possible_pairs = find_kernel_stride_pairs(H_in, H_out)
-print(possible_pairs)
-# output_size(61, 4, 3, 1)
 #%% max pool to reduce dimensions (preserves extremes)
 pool = 3
 r = ds.rolling(latitude=pool, longitude=pool)
@@ -76,6 +59,7 @@ yearn = ds['time'].dt.year.values.max()
 assert ds.u10.values.max() == wind_max, "Check max wind speed not smoothened"
 assert ds.msl.values.min() == pressure_min, "Check min pressure not smoothened"
 ds.to_netcdf(os.path.join(outdir, f"data_{year0}_{yearn}.nc"))
+ds # check out dimensions etc
 # %%
 # check looks okay over spatial domain
 fig, axs = plt.subplots(1, 2, figsize=(10, 4))
@@ -84,7 +68,8 @@ ds.isel(time=0).msl.plot(ax=axs[1])
 axs[0].set_title("10m wind")
 axs[1].set_title("sea-level pressure")
 
-# %% turn to dataframe --maybe change R code to be ncdf compatible later
+# %% TODO: Fix this part so only working with NetCDF
+# turn to dataframe --maybe change R code to be ncdf compatible later (this takes a minute)
 df['i'] = df['latitude'].rank(method='dense').astype(int)
 df['j'] = df['longitude'].rank(method='dense').astype(int)
 df['grid'] = df[['i', 'j']].apply(tuple, axis=1).rank(method='dense').astype(int)
@@ -93,7 +78,6 @@ df = df.drop(columns=['latitude', 'longitude']).reset_index()
 df.reset_index().to_parquet(os.path.join(outdir, f"data_{year0}_{yearn}.parquet"))
 # %% saffir-simpson scale
 # https://www.nhc.noaa.gov/aboutsshws.php
-
 
 ss_u10 = np.array([16, 29, 38, 44, 52, 63])
 ss_msl = np.array([925, 945, 960, 975, 990, 1005]) * 100
