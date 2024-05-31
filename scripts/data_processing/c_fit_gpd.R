b06f34b8860cbc3312be0b603e4781e88fd7e660
Alison Peard <alison.peard@gmail.com>
2024-05-31 14:31:27 +0100

    load ncdf to fit GPD in R

--- scripts/data_processing/c_fit_gpd.R
+++ scripts/data_processing/c_fit_gpd.R
@@ -1,3 +1,6 @@
+# Identify events and fit GPD to componentwise maxima. Also assign RP to each event
+# INPUT: data_1950_2022.nc
+# OUTPUT: (1) event_data.parquet, (2) fitted_data.parquet, (3) monthly_medians.csv
 # https://cran.r-project.org/web/packages/eva/eva.pdf
 rm(list = ls())
 library(eva)
@@ -6,8 +9,11 @@ library(lubridate)
 library(dplyr)
 require(ggplot2)
 library(extRemes)
-filename <- 'data_1950_2022.parquet'
+library(CFtime)
+library(tidync)
+
 wd <- "/Users/alison/Documents/DPhil/multivariate"
+filename <- 'data_1950_2022.nc'
 indir <- paste0(wd, '/', 'era5_data')
 r.func <- max # https://doi.org/10.1111/rssb.12498
 ########### DEFINE FUNCTIONS ###################################################
@@ -36,89 +42,115 @@ progress_bar <- function(n, prefix="", suffix="") {
   }
 }
 ########### LOAD DATA AND STANDARDISE IT #######################################
-wind.df <- read_parquet(paste0(indir, '/', filename))
-wind.df$msl <- -wind.df$msl # negate msl so maximising both vars
-wind.df = wind.df[,c('grid', 'time', 'u10', 'msl')]
-wind.df$time <- as.Date(wind.df$time)
-medians <- monthly.medians(wind.df, 'u10')
-medians$mslp <- monthly.medians(wind.df, 'msl')$msl
-wind.df$u10 <- standardise.by.month(wind.df, 'u10')
-wind.df$msl <- standardise.by.month(wind.df, 'msl')
-########### DEFINE EVENTS ######################################################
-wind.df$u10 <- scale(wind.df$u10)
-wind.df$msl <- scale(wind.df$msl)
-df <- aggregate(. ~ time, wind.df[,c('time', 'u10', 'msl')], r.func)
-colnames(df) <- c('time', 'u10', 'msl')
-df$ecdf <- get.ecdf(apply(df[,c('u10', 'msl')], 1, r.func)); hist(df$ecdf)
+src <- tidync(paste0(indir, '/', filename))
+era5.df <- src %>% hyper_tibble(force = TRUE)
+coords <- src %>% activate('grid') %>% hyper_tibble(force = TRUE)
+era5.df <- left_join(era5.df, coords, by=c('longitude', 'latitude'))
+rm(coords)
+
+era5.df$msl <- -era5.df$msl # negate msl so maximising both vars
+era5.df = era5.df[,c('grid', 'time', 'u10', 'msl')]
+
+era5.df$time <- as.Date(CFtimestamp(CFtime("days since 1950-01-01", "gregorian", era5.df$time)))
+medians <- monthly.medians(era5.df, 'u10')
+medians$mslp <- monthly.medians(era5.df, 'msl')$msl
+era5.df$u10 <- standardise.by.month(era5.df, 'u10')
+era5.df$msl <- standardise.by.month(era5.df, 'msl')
+########### DECLUSTER EVENTS ###################################################
+df <- aggregate(. ~ time, era5.df[,c('time', 'u10')], r.func) # overall max wind each day
 
 # search for threshold that gives most data
-qs <- c(60:99) / 100
+qs <-c(60:99) / 100
 rs <- c(1:14)
 nclusters <- matrix(nrow=length(rs), ncol=length(qs))
 ext.ind <- matrix(nrow=length(rs), ncol=length(qs))
+p.vals <- matrix(nrow=length(rs), ncol=length(qs))
 for(i in 1:length(rs)){
   for(j in 1:length(qs)){
-    thresh <- quantile(df$ecdf, qs[j])
-    d <- decluster(df$ecdf, thresh=thresh, r=rs[i], method='runs')
-    e <- extremalindex(c(d), thresh, r=rs[i], method='runs')
+    thresh <- quantile(df$u10, qs[j])
+    d <- decluster(df$u10, thresh=thresh, r=rs[i], method='runs')
+    e <- extremalindex(c(d), thresh, r=rs[i], method='runs') # note this is θ=1 a lot, maybe double-check
+    p <- Box.test(c(d)[c(d) > thresh], type='Ljung') # independence of exceedences
     nclusters[i, j] <- e[['number.of.clusters']]
     ext.ind[i,j] <- e[['extremal.index']]
+    p.vals[i, j] <- p$p.value
   }
 }
-nclusters[ext.ind < 0.8] <- 0
+# remove any cases with dependence in exceedences
+nclusters[ext.ind < 0.8] <- 0 # see ?extremalindex, if theta < 1, then there is some dependency (clustering) in the limit
+nclusters[p.vals < 0.1] <- 0  #  
 ind <- which(nclusters == max(nclusters), arr.ind=TRUE)
 r <- rs[ind[1]]
 q <- qs[ind[2]]
 
-thresh <- quantile(df$ecdf, q)
-declustering <- decluster(df$ecdf, thresh, r=r)
-times <- df$time[df$ecdf > thresh] # times when joint extremeness exceeds thresh
-ecdfs <- df$ecdf[df$ecdf > thresh]
-clusters <- attr(declustering, 'clusters')
-cluster.df <- data.frame(time=times, cluster=clusters, ecdf=ecdfs)
-cluster.df <- cluster.df %>%
-  group_by(cluster) %>%
-  mutate(cluster.size = n()) %>%
-  mutate(ecdf = mean(ecdf)) %>%
-  ungroup()
-cluster.df$q <- q
-########### FIT GPD TO EVENT MAXIMA ############################################
-wind.df.all <- read_parquet(paste0(indir, '/', filename))
-wind.df.all$msl <- -wind.df.all$msl # negate msl again...
-wind.df.all = wind.df.all[,c('grid', 'time', 'u10', 'msl')]
-wind.df.all$time <- as.Date(wind.df.all$time)
-wind.df.all$u10 <- standardise.by.month(wind.df.all, 'u10')
-wind.df.all$msl <- standardise.by.month(wind.df.all, 'msl')
-grid.cells <- unique(wind.df.all$grid)
-ngrid <- length(grid.cells)
+# make dataframe of events
+thresh <- quantile(df$u10, q)
 
-# NOTE: run again to check whether these are needed and delete if not
-# p.vals <- vector(length=ngrid)
-# threshs <- vector(length=ngrid)
-# shape.params <- vector(length=ngrid)
-# scale.params <- vector(length=ngrid)
+plot(df$u10)
+abline(h=thresh, col='red')
+
+declustering <- decluster(df$u10, thresh, r=r)
+clusters <- attr(declustering, 'clusters')
+times <- df$time[df$u10 > thresh] # times when max(u10) exceeds thresh
+u10 <- df$u10[df$u10 > thresh]
+cluster.df <- data.frame(time=times, storm=clusters, u10=u10)
+
+# cluster stats
+cluster.grouped <- cluster.df %>%
+  group_by(storm) %>%
+  mutate(storm.size = n()) %>%
+  slice(which.max(u10)) %>%
+  summarise(u10 = max(u10),
+            time = time,
+            storm.size = storm.size)
+
+# empirical return period calculation
+m <- nrow(cluster.grouped); m
+nyears <- length(unique(year(era5.df$time)))
+lambda <- m / nyears; lambda # yearly occurrence rate
+p <- 1 - (rank(cluster.grouped$u10, ties.method='average')) / (m + 1) # exceedence probability
+rp <- 1 / (lambda*p)
+cluster.grouped$storm.rp <- rp
+hist(rp, breaks=50)
+
+cluster.df <- left_join(cluster.df, cluster.grouped[c('storm', 'storm.rp', 'storm.size')], by=c('storm'))
+cluster.df$q <- q
+head(cluster.df[order(-cluster.df$storm.rp),])
+########### FIT GPD TO EVENT MAXIMA ############################################
+src <- tidync(paste0(indir, '/', filename))
+era5.df.all <- src %>% hyper_tibble()
+coords <- src %>% activate('grid') %>% hyper_tibble()
+era5.df.all <- left_join(era5.df.all, coords, by=c('longitude', 'latitude'))
+era5.df.all$time <- as.Date(CFtimestamp(CFtime("days since 1950-01-01", "gregorian", era5.df.all$time)))
+rm(coords)
+
+era5.df.all$msl <- -era5.df.all$msl # negate msl again...
+era5.df.all = era5.df.all[,c('grid', 'time', 'u10', 'msl')]
+era5.df.all$time <- as.Date(era5.df.all$time)
+era5.df.all$u10 <- standardise.by.month(era5.df.all, 'u10')
+era5.df.all$msl <- standardise.by.month(era5.df.all, 'msl')
+grid.cells <- unique(era5.df.all$grid)
+ngrid <- length(grid.cells)
 
 # fit GPD models to excesses
 q <- 0.8
 update_progress <- progress_bar(ngrid, "Fitting GPD to wind excesses:", "Complete")
-wind.transformed.df <- data.frame()
+fields <- c("storm", "u10", "time", "storm.rp", "grid", "thresh", "scale", "shape", "p", "ecdf")
+wind.transformed.df <- data.frame(matrix(nrow=0, ncol=length(fields)))
+colnames(wind.transformed.df) <- fields
 for(i in 1:ngrid){
   GRIDCELL <- grid.cells[i]
-  # process wind
-  wind.df <- wind.df.all[wind.df.all$grid == GRIDCELL,]
-  wind.df <- wind.df[wind.df$time %in% times,]
   
-  wind.df <- left_join(wind.df, cluster.df[,c('time', 'cluster', 'ecdf')], by=c('time'='time'))
-  colnames(wind.df)[colnames(wind.df) == 'ecdf'] <- 'extremeness'
-  #wind.df$cluster <- cluster.df$cluster
-  #wind.df$extremeness <- cluster.df$ecdf
-
-  maxima <- wind.df %>%
-    group_by(cluster) %>%
+  # process wind
+  era5.df <- era5.df.all[era5.df.all$grid == GRIDCELL,]
+  era5.df <- era5.df[era5.df$time %in% times,]
+  era5.df <- left_join(era5.df, cluster.df[,c('time', 'storm', 'storm.rp')], by=c('time'='time'))
+  maxima <- era5.df %>%
+    group_by(storm) %>%
     slice(which.max(u10)) %>%
     summarise(u10 = max(u10),
               time = time,
-              extremeness = extremeness,
+              storm.rp = storm.rp,
               grid = grid)
   thresh.u10 <- quantile(maxima$u10, q)
   
@@ -128,7 +160,7 @@ for(i in 1:ngrid){
   hist(excesses)
   
   # fit models...
-  tryCatch({
+  new.row <- tryCatch({
     fit.u10 <- gpdAd(maxima$u10[maxima$u10 >= thresh.u10],
                      bootstrap = TRUE, bootnum = 10,
                      allowParallel = TRUE, numCores=2) # H0: GPD distribution
@@ -145,31 +177,33 @@ for(i in 1:ngrid){
     f.exceed <- 1 - ecdf(maxima$u10)(thresh.u10)
     survival.probs <- f.exceed * (1 - pgpd(maxima$u10[maxima$u10 >= thresh.u10], thresh.u10, scale, shape))
     maxima$ecdf[maxima$u10 >= thresh.u10] <- 1 - survival.probs
-    wind.transformed.df <- rbind(wind.transformed.df, maxima)
+    maxima
     
   }, error=function(e){
-    print(paste0("skipping grid cell ", GRIDCELL, e))
+    print(paste0("skipping grid cell ", GRIDCELL, ' ', e))
+    missing.columns <- setdiff(names(wind.transformed.df), names(maxima))
+    maxima[missing.columns] <- NA
+    return(maxima)
   })
+  wind.transformed.df <- rbind(wind.transformed.df, new.row)
   update_progress(i)
 }
 
-mslp.transformed.df <- data.frame()
+mslp.transformed.df <- data.frame(matrix(nrow=0, ncol=length(fields)))
+colnames(mslp.transformed.df) <- fields
 update_progress <- progress_bar(ngrid, "Fitting GPD to MSLP excesses:", "Complete")
 for(i in 1:ngrid){
   GRIDCELL <- grid.cells[i]
-  wind.df <- wind.df.all[wind.df.all$grid == GRIDCELL,]
-  wind.df <- wind.df[wind.df$time %in% times,]
-  wind.df <- left_join(wind.df, cluster.df[,c('time', 'cluster', 'ecdf')], by=c('time'='time'))
-  colnames(wind.df)[colnames(wind.df) == 'ecdf'] <- 'extremeness'
-  #wind.df$cluster <- cluster.df$cluster
-  #wind.df$extremeness <- cluster.df$ecdf
+  era5.df <- era5.df.all[era5.df.all$grid == GRIDCELL,]
+  era5.df <- era5.df[era5.df$time %in% times,]
+  era5.df <- left_join(era5.df, cluster.df[,c('time', 'storm', 'storm.rp')], by=c('time'='time'))
   
-  maxima <- wind.df %>%
-    group_by(cluster) %>%
+  maxima <- era5.df %>%
+    group_by(storm) %>%
     slice(which.max(msl)) %>%
     summarise(msl = max(msl),
               time = time,
-              extremeness = extremeness,
+              storm.rp = storm.rp,
               grid = grid)
   thresh.msl <- quantile(maxima$msl, q)
   
@@ -179,7 +213,7 @@ for(i in 1:ngrid){
   hist(excesses)
   
   # fit models...
-  tryCatch({
+  new.row <- tryCatch({
     fit.msl <- gpdAd(maxima$msl[maxima$msl >= thresh.msl],
                      bootstrap = TRUE, bootnum = 10,
                      allowParallel = TRUE, numCores=2) # H0: GPD distribution
@@ -196,25 +230,30 @@ for(i in 1:ngrid){
     f.exceed <- 1 - ecdf(maxima$msl)(thresh.msl)
     survival.probs <- 1 - pgpd(maxima$msl[maxima$msl >= thresh.msl], thresh.msl, scale, shape)
     maxima$ecdf[maxima$msl >= thresh.msl] <- 1 - f.exceed * survival.probs
-    mslp.transformed.df <- rbind(mslp.transformed.df, maxima)
+    maxima
     
   }, error=function(e){
     print(paste0("skipping grid cell ", GRIDCELL, e))
+    missing.columns <- setdiff(names(mslp.transformed.df), names(maxima))
+    maxima[missing.columns] <- NA
+    return(maxima)
   })
+  mslp.transformed.df <- rbind(mslp.transformed.df, new.row)
   update_progress(i)
 } 
 
 transformed.df <- wind.transformed.df %>% inner_join(mslp.transformed.df,
-                                                     by = c('grid', 'cluster'),
+                                                     by = c('grid', 'storm'),
                                                      suffix = c('.u10', '.mslp'))
 transformed.df$thresh.q <- q # approx. extremeness measure
+
 ########### SAVE TO CSV FOR PYTHON #############################################
 write.csv(medians, paste0(indir, '/', 'monthly_medians.csv'), row.names=FALSE)
 write_parquet(cluster.df, paste0(indir, '/', 'event_data.parquet'))
 write_parquet(transformed.df, paste0(indir, '/', 'fitted_data.parquet'))
 
 print(paste0("Saved as ", indir, '/', 'fitted_data.csv.'))
-print(paste0(length(unique(transformed.df$cluster)), " events processed."))
+print(paste0(length(unique(transformed.df$storm)), " events processed."))
 ########### FIGURES ############################################################
 GRIDCELL <- 15
 grid.df <- transformed.df[transformed.df$grid == GRIDCELL,]
@@ -223,3 +262,11 @@ acf(grid.df$u10, main="U10 cluster maxima ACF")
 pacf(grid.df$u10, main="U10 cluster maxima PACF")
 acf(grid.df$msl, main="MSLP cluster maxima ACF")
 pacf(grid.df$msl, main="MSLP cluster maxima PACF")
+
+
+
+#####
+missing.days <- (nyears * 365) - length(unique(era5.df.all$time))
+missing.years <- missing.days / 365
+
+
