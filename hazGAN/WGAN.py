6bbc2266819b270cf6c021ab4e47866ebd99d658
Alison Peard <alison.peard@gmail.com>
2024-06-24 13:02:02 +0100

    added changeable interpolation

--- hazGAN/WGAN.py
+++ hazGAN/WGAN.py
@@ -56,27 +56,27 @@ def define_generator(config, nchannels=2):
     z = tf.keras.Input(shape=(100,))
 
     # First fully connected layer, 1 x 1 x 25600 -> 5 x 5 x 1024
-    fc = layers.Dense(config["complexity_0"] * config["g_layers"][0])(z)
-    fc = layers.Reshape((5, 5, int(config["complexity_0"] * config["g_layers"][0] / 25)))(fc)
+    fc = layers.Dense(config["g_complexity"] * config["g_layers"][0])(z)
+    fc = layers.Reshape((5, 5, int(config["g_complexity"] * config["g_layers"][0] / 25)))(fc)
     lrelu0 = layers.LeakyReLU(config.lrelu)(fc)
     drop0 = layers.Dropout(config.dropout)(lrelu0)
     bn0 = layers.BatchNormalization(axis=-1)(drop0)  # normalise along features layer (1024)
 
     # Deconvolution, 5 x 5 x 1024 -> 7 x 7 x 512
-    conv1 = layers.Conv2DTranspose(config["complexity_1"] * config["g_layers"][1], 3, 1, use_bias=False)(bn0)
+    conv1 = layers.Conv2DTranspose(config["g_complexity"] * config["g_layers"][1], 3, 1, use_bias=False)(bn0)
     lrelu1 = layers.LeakyReLU(config.lrelu)(conv1)
     drop1 = layers.Dropout(config.dropout)(lrelu1)
     bn1 = layers.BatchNormalization(axis=-1)(drop1)
 
     # Deconvolution, 6 x 8 x 512 -> 14 x 18 x 256
-    conv2 = layers.Conv2DTranspose(config["complexity_2"] * config["g_layers"][2], (3, 4), 1, use_bias=False)(bn1)
+    conv2 = layers.Conv2DTranspose(config["g_complexity"] * config["g_layers"][2], (3, 4), 1, use_bias=False)(bn1)
     lrelu2 = layers.LeakyReLU(config.lrelu)(conv2)
     drop2 = layers.Dropout(config.dropout)(lrelu2)
     bn2 = layers.BatchNormalization(axis=-1)(drop2)
 
     # Output layer, 17 x 21 x 128 -> 20 x 24 x nchannels
     #score = layers.Conv2DTranspose(nchannels, (4, 6), 2)(bn2)
-    conv3 = layers.Resizing(20, 24, interpolation='lanczos3')(bn2)
+    conv3 = layers.Resizing(20, 24, interpolation=config.interpolation)(bn2)
     score = layers.Conv2DTranspose(nchannels, (4, 6), 1, padding='same')(conv3)
     o = score if config.gumbel else tf.keras.activations.sigmoid(score) # NOTE: check
     return tf.keras.Model(z, o, name="generator")
@@ -90,22 +90,22 @@ def define_critic(config, nchannels=2):
     x = tf.keras.Input(shape=(20, 24, nchannels))
 
     # 1st hidden layer 9x10x64
-    conv1 = layers.Conv2D(config["d_layers"][0], (4, 5), (2, 2), "valid", kernel_initializer=tf.keras.initializers.GlorotUniform())(x)
+    conv1 = layers.Conv2D(config["d_complexity"] * config["d_layers"][0], (4, 5), (2, 2), "valid", kernel_initializer=tf.keras.initializers.GlorotUniform())(x)
     lrelu1 = layers.LeakyReLU(config.lrelu)(conv1)
     drop1 = layers.Dropout(config.dropout)(lrelu1)
 
     # 2nd hidden layer 7x7x128
-    conv1 = layers.Conv2D(config["d_layers"][1], (3, 4), (1, 1), "valid", use_bias=False)(drop1)
+    conv1 = layers.Conv2D(config["d_complexity"] * config["d_layers"][1], (3, 4), (1, 1), "valid", use_bias=False)(drop1)
     lrelu2 = layers.LeakyReLU(config.lrelu)(conv1)
     drop2 = layers.Dropout(config.dropout)(lrelu2)
 
     # 3rd hidden layer 5x5x256
-    conv2 = layers.Conv2D(config["d_layers"][2], (3, 3), (1, 1), "valid", use_bias=False)(drop2)
+    conv2 = layers.Conv2D(config["d_complexity"] * config["d_layers"][2], (3, 3), (1, 1), "valid", use_bias=False)(drop2)
     lrelu3 = layers.LeakyReLU(config.lrelu)(conv2)
     drop3 = layers.Dropout(config.dropout)(lrelu3)
 
     # fully connected 1x1
-    flat = layers.Reshape((-1, 5 * 5 * config["d_layers"][2]))(drop3)
+    flat = layers.Reshape((-1, 5 * 5 * config["d_complexity"] * config["d_layers"][2]))(drop3)
     score = layers.Dense(1)(flat)
     out = layers.Reshape((1,))(score)
     return tf.keras.Model(x, out, name="critic")
@@ -117,7 +117,7 @@ class WGAN(keras.Model):
         self.critic = define_critic(config, nchannels)
         self.generator = define_generator(config, nchannels)
         self.latent_dim = config.latent_dims
-        self.lambda_ = config.lambda_
+        self.lambda_chi = config.lambda_chi
         self.lambda_gp = config.lambda_gp
         self.gumbel = config.gumbel
         self.config = config
@@ -181,7 +181,7 @@ class WGAN(keras.Model):
             generated_data = self.generator(random_latent_vectors)
             score = self.critic(generated_data, training=False)
             g_loss_raw = -tf.reduce_mean(score)
-            #g_penalty = self.lambda_ * chi_loss(data, generated_data) # FIX: extremal correlation structure
+            #g_penalty = self.lambda_chi * chi_loss(data, generated_data) # FIX: extremal correlation structure
             g_loss = g_loss_raw #+ g_penalty 
         grads = tape.gradient(g_loss, self.generator.trainable_weights)
         self.g_optimizer.apply_gradients(zip(grads, self.generator.trainable_weights))
