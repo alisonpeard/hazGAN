0595db199de7c92ada1e353c4b8ed504b50df93e
alisonpeard <alison.peard@gmail.com>
2024-06-28 15:55:44 +0000

    pass flags to sbatch manager

--- scripts/model_fitting/train-sbatch.sh
+++ scripts/model_fitting/train-sbatch.sh
@@ -2,10 +2,10 @@
 #SBATCH --job-name=hazGAN_sweep
 #SBATCH --output=sbatch/hazGAN_sweep_%A_%a.out
 #SBATCH --error=sbatch/hazGAN_sweep_%A_%a.err
-#SBATCH --array=1-5
+#SBATCH --array=1-10
 #SBATCH --time=10:00:00
 
 echo "TASK ID: " $SLURM_ARRAY_TASK_ID
 # RUN AGENT: (e.g., wandb agent alison-peard/hazGAN/zl1ugbxw)
-wandb agent alison-peard/hazGAN/5isapa89
+wandb agent "alison-peard/hazGAN/$1"
 # INITIALISE SWEEP: wandb sweep sweep.yaml
