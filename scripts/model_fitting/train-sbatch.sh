b45df67d963d48b5c1542d71e1cf1c6af62e75a6
alisonpeard <alison.peard@gmail.com>
2024-06-28 15:25:42 +0000

    unsure what changes these are

--- scripts/model_fitting/train-sbatch.sh
+++ scripts/model_fitting/train-sbatch.sh
@@ -2,10 +2,10 @@
 #SBATCH --job-name=hazGAN_sweep
 #SBATCH --output=sbatch/hazGAN_sweep_%A_%a.out
 #SBATCH --error=sbatch/hazGAN_sweep_%A_%a.err
-#SBATCH --array=1-2
-#SBATCH --time=1:00:00
+#SBATCH --array=1-5
+#SBATCH --time=10:00:00
 
 echo "TASK ID: " $SLURM_ARRAY_TASK_ID
-# RUN AGENT: wandb agent alison-peard/hazGAN/zl1ugbxw
-
+# RUN AGENT: (e.g., wandb agent alison-peard/hazGAN/zl1ugbxw)
+wandb agent alison-peard/hazGAN/5isapa89
 # INITIALISE SWEEP: wandb sweep sweep.yaml
