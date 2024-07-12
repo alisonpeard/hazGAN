b156c498c08b2c707043378ae64fcbfe939943a1
Alison Peard <alison.peard@gmail.com>
2024-07-12 14:45:41 +0100

    removed old conv2dtranspose layer

--- hazGAN/WGAN.py
+++ hazGAN/WGAN.py
@@ -85,7 +85,6 @@ def define_generator(config, nchannels=2):
     bn2 = layers.BatchNormalization(axis=-1)(drop2)
 
     # Output layer, 17 x 21 x 128 -> 20 x 24 x nchannels
-    #score = layers.Conv2DTranspose(nchannels, (4, 6), 2)(bn2)
     conv3 = layers.Resizing(20, 24, interpolation=config.interpolation)(bn2)
     score = layers.Conv2DTranspose(nchannels, (4, 6), 1, padding='same')(conv3)
     o = score if config.gumbel else tf.keras.activations.sigmoid(score) # NOTE: check
