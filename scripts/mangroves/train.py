898609c9b2928775e80a087935974c580f79e43a
Alison Peard <alison.peard@gmail.com>
2024-07-22 16:58:19 +0100

    Trying out new ERA5 data to train RF

--- scripts/mangroves/train.py
+++ scripts/mangroves/train.py
@@ -23,20 +23,30 @@ import matplotlib.pyplot as plt
 
 bob_crs = 24346
 # %%
-path = '/Users/alison/Documents/DPhil/paper1.nosync/mangrove_data/v3__mine/era5_and_slope_0.csv'
+gdf = gpd.read_file('/Users/alison/Documents/DPhil/paper1.nosync/mangrove_data/v3__mine/data_with_slopes.gpkg')
+df = pd.read_csv('/Users/alison/Documents/DPhil/paper1.nosync/mangrove_data/v3__mine/data_with_era5.csv')
+df['storm'] = df['storm'].str.capitalize()
+# %%
+final_df = gdf.merge(df, left_on=['center_centerLat', 'center_centerLon', 'stormName'], right_on=['lat', 'lon', 'storm'], how='left')
+final_df = final_df.dropna()
+final_df.to_csv('/Users/alison/Documents/DPhil/paper1.nosync/mangrove_data/v3__mine/final.csv', index=False)
+# %%
+path = '/Users/alison/Documents/DPhil/paper1.nosync/mangrove_data/v3__mine/final.csv'
 df = pd.read_csv(path)
 [*df.columns]
 # %% ------Train a model------
 # TODO: pressure, precipitation checks
 regressors = [
-    # 'slope',
+    'slope',
     # 'landingWindMaxLocal2',
-    # 'landingPressure'
-    'wind',
-    'mslp',
+    # 'landingPressure',
+    'era5_wind',
+    'era5_pressure',
+    # 'era5_precip',
     # 'stormFrequency_mean',
     # 'elevation_mean',
     # 'totalPrec_total',
+    # 'bathymetry_mean',
     # 'landingSpeed',
     # 'coastDist'
     ]
@@ -48,19 +58,64 @@ gdf = gdf.dropna()
 X = gdf[regressors]
 y = gdf[response]
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+# %%
+if True:
+  import seaborn as sns
+
+  sns.pairplot(gdf, x_vars=regressors, y_vars=response)
+  sns.pairplot(X)
+# %% 
+
+from sklearn.preprocessing import StandardScaler
+scaler = StandardScaler()
+X_train = scaler.fit_transform(X_train)
+X_test = scaler.transform(X_test)
 # %% train a random forest
-model = RandomForestRegressor(n_estimators=100 * len(regressors), random_state=42).fit(X_train, y_train)
+for i in [1, 2, 5, 10, 20, 50, 100, 200]:
+  model = RandomForestRegressor(n_estimators=i * len(regressors), random_state=42).fit(X_train, y_train)
+
+  y_pred = model.predict(X_test)
+  rmse = root_mean_squared_error(y_test, y_pred)
+  rsq = r2_score(y_test, y_pred)
+
+  print(f'RMSE: {rmse:.4f}, R^2: {rsq:.4f}, nestimars factor: {i})')
+# %%
+# import simple mlp
+from sklearn.neural_network import MLPRegressor
+# %%
+# try svm, xgboost, gaussian process regressors
+from sklearn.svm import SVR
+from xgboost import XGBRegressor
+from sklearn.gaussian_process import GaussianProcessRegressor
+from sklearn.gaussian_process.kernels import DotProduct, WhiteKernel, RBF, Matern
+
+# %%
+# model = XGBRegressor().fit(X_train, y_train)
+# model = SVR().fit(X_train, y_train)
+model = GaussianProcessRegressor(kernel=DotProduct() + WhiteKernel()).fit(X_train, y_train)
+# model = RandomForestRegressor().fit(X_train, y_train)
+# model = MLPRegressor(hidden_layer_sizes=(100, 100)).fit(X_train, y_train)
+# model = MLPRegressor(hidden_layer_sizes=(100, 100, 100)).fit(X_train, y_train)
+# model = MLPRegressor(hidden_layer_sizes=(100, 100, 100, 100)).fit(X_train, y_train)
+# model = MLPRegressor(hidden_layer_sizes=(100, 100, 100, 100, 100)).fit(X_train, y_train)
+# model = MLPRegressor().fit(X_train, y_train)
+
+# other variations of the gaussian process regressor
+# model = GaussianProcessRegressor(kernel=RBF() + WhiteKernel()).fit(X_train, y_train)
+model = GaussianProcessRegressor(kernel=Matern() + WhiteKernel()).fit(X_train, y_train)
+
 
 y_pred = model.predict(X_test)
-root_mean_squared_error(y_test, y_pred)
+rmse = root_mean_squared_error(y_test, y_pred)
+rsq = r2_score(y_test, y_pred)
+print(f'RMSE: {rmse:.4f}, R^2: {rsq:.4f}')
 # %%
-
 fig, ax = plt.subplots()
 ax.scatter(y_test, y_pred, color='k')
 ax.plot(y_test, y_test, color='r', label='test')
 ax.set_xlabel('Observed')
 ax.set_ylabel('Predicted')
-ax.set_title(f'R^2 = {r2_score(y_test, y_pred):.4f}')
+ax.set_title(f'R^2 = {rsq:.4f}')
 # %% plot the feature importances
 importances = model.feature_importances_
 indices = np.argsort(importances)[::-1]
