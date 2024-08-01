057c45f9449f34a67507e0af4811de56d0797003
alisonpeard <alison.peard@gmail.com>
2024-08-01 01:41:35 +0000

    cluster sweep config

--- scripts/training/train_sbatch.sh
+++ scripts/training/train_sbatch.sh
@@ -2,10 +2,10 @@
 #SBATCH --job-name=hazGAN
 #SBATCH --output=sbatch/hazGAN_%A_%a.out
 #SBATCH --error=sbatch/hazGAN_%A_%a.err
-#SBATCH --array=1-50%10
+#SBATCH --array=1-100%5
 #SBATCH --partition=GPU
 #SBATCH --gres=gpu:tesla:1
-#SBATCH --time=04:00:00
+#SBATCH --time=03:00:00
 
 
 echo "TASK ID: " $SLURM_ARRAY_TASK_ID
@@ -16,6 +16,7 @@ wandb agent alison-peard/hazGAN/$1
 # $ micromamba activate hazGAN
 # $ wandb sweep sweep.yaml
 # $ sbatch train_sbatch.sh <sweep id>
+
 # if not working with sbatch, run
 # srun -p GPU --gres=gpu:tesla:1 --time=04:00:00 --pty wandb agent alison-peard/hazGAN/8yep8d6w
 # precipitation sweep: gfkbsk3l
