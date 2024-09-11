905fba5acb1409db8a3c7fabd6c0e798d931b51b
alisonpeard <alison.peard@gmail.com>
2024-09-11 12:45:07 +0000

    sweep config

--- scripts/training/train_sbatch.sh
+++ scripts/training/train_sbatch.sh
@@ -2,10 +2,10 @@
 #SBATCH --job-name=hazGAN
 #SBATCH --output=sbatch/hazGAN_%A_%a.out
 #SBATCH --error=sbatch/hazGAN_%A_%a.err
-#SBATCH --array=1-100%5
+#SBATCH --array=1-100%1
 #SBATCH --partition=GPU
-#SBATCH --gres=gpu:tesla:1
-#SBATCH --time=03:00:00
+#SBATCH --gres=gpu:3080ti:1
+#SBATCH --time=10:00:00
 
 
 echo "TASK ID: " $SLURM_ARRAY_TASK_ID
@@ -18,5 +18,7 @@ wandb agent alison-peard/hazGAN/$1
 # $ sbatch train_sbatch.sh <sweep id>
 
 # if not working with sbatch, run
-# srun -p GPU --gres=gpu:tesla:1 --time=04:00:00 --pty wandb agent alison-peard/hazGAN/8yep8d6w
-# precipitation sweep: gfkbsk3l
+# srun -p GPU --gres=gpu:tesla:1 --time=04:00:00 --pty wandb agent alison-peard/hazGAN/bodaevqp
+# GPUs: tesla, 3080ti, 1080ti
+# latest sweep: s9k8wgnb
+# e.g., sbatch train_sbatch.sh s9k8wgnb
