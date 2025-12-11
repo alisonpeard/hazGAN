a14493c1be00620c6e140c20363b7fa117877b72
alisonpeard <alison.peard@gmail.com>
2025-12-11 16:22:56 +0000

    arc slurm scripts

---
+++ scripts/02_training_inference/03_train-arc.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+#SBATCH --job-name=train
+#SBATCH --output=./train.out
+#SBATCH --error=./train.err
+#SBATCH --partition="short,interactive,medium"
+#SBATCH --gres=gpu:v100:1
+#SBATCH --time=06:00:00
+#SBATCH --mail-type=END,FAIL
+#SBATCH --mail-user=spet5107@ox.ac.uk
+#SBATCH --mem=24G
+#SBATCH --cpus-per-task=4
+
+DATA=/data/ouce-opsis/spet5107/data/gaussian.zip
+OUTDIR=/data/ouce-opsis/spet5107/data
+
+module load Anaconda3
+conda activate /data/ouce-opsis/spet5107/hazGAN2/.snakemake/conda/55ad1cb60ae140a2919a9f3f8906a963_ # styleGAN snakemake env
+
+source /data/ouce-opsis/spet5107/hazGAN2/workflow/scripts/cuda_env.sh
+
+mkdir -p $OUTDIR
+
+python /data/ouce-opsis/spet5107/hazGAN2/workflow/src/stylegan/train.py \
+    --outdir=$OUTDIR \
+    --data=$DATA \
+    --gpus=1 \
+    --DiffAugment="color,translation,cutout" \
+    --kimg=300
+
+# find $OUTDIR -name "network-snapshot-*.pkl" -not -name "network-snapshot-00300.pkl" -delete
