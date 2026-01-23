5a9e7896d46dd32b4600d5a7846ccda35bfee8f8
alisonpeard <alison.peard@gmail.com>
2026-01-23 00:36:14 +0000

    arc snapshot

---
+++ scripts/02_training_inference/04_inference-arc-rescale.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+#SBATCH --job-name=gen
+#SBATCH --output=./gen_rescale.out
+#SBATCH --error=./gen_rescale.err
+#SBATCH --partition="short,medium,interactive"
+#SBATCH --gres=gpu:v100:1
+#SBATCH --time=01:00:00
+#SBATCH --mail-type=END,FAIL
+#SBATCH --mail-user=spet5107@ox.ac.uk
+#SBATCH --mem=24G
+#SBATCH --cpus-per-task=4
+
+OUTDIR="/data/ouce-opsis/spet5107/data/generated/rescaled_minmax09"
+NETWORK="/data/ouce-opsis/spet5107/data/00005-rescaled_minmax09-low_shot-kimg300-color-translation-cutout/network-snapshot-000300.pkl"
+SCRIPT="/data/ouce-opsis/spet5107/hazGAN/styleGAN-DA/src/generate.py"
+
+module load Anaconda3
+source $(conda info --base)/etc/profile.d/conda.sh
+ENV="/data/ouce-opsis/spet5107/hazGAN2/.snakemake/conda/55ad1cb60ae140a2919a9f3f8906a963_"
+
+mkdir -p $OUTDIR
+
+conda run --prefix $ENV python ${SCRIPT} \
+  --outdir=${OUTDIR} \
+  --seeds=1-914 \
+  --trunc=1.0 \
+  --network=${NETWORK}
---
+++ scripts/02_training_inference/04_inference-arc-uniform.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+#SBATCH --job-name=gen
+#SBATCH --output=./gen_unif.out
+#SBATCH --error=./gen_unif.err
+#SBATCH --partition="short,medium,interactive"
+#SBATCH --gres=gpu:v100:1
+#SBATCH --time=01:00:00
+#SBATCH --mail-type=END,FAIL
+#SBATCH --mail-user=spet5107@ox.ac.uk
+#SBATCH --mem=24G
+#SBATCH --cpus-per-task=4
+
+OUTDIR="/data/ouce-opsis/spet5107/data/generated/uniform_minmax09"
+NETWORK="/data/ouce-opsis/spet5107/data/00006-uniform_minmax09-low_shot-kimg300-color-translation-cutout/network-snapshot-000300.pkl"
+SCRIPT="/data/ouce-opsis/spet5107/hazGAN/styleGAN-DA/src/generate.py"
+
+module load Anaconda3
+source $(conda info --base)/etc/profile.d/conda.sh
+conda activate /data/ouce-opsis/spet5107/hazGAN2/.snakemake/conda/55ad1cb60ae140a2919a9f3f8906a963_ # styleGAN snakemake env
+
+mkdir -p $OUTDIR
+
+python ${SCRIPT} \
+  --outdir=${OUTDIR} \
+  --seeds=1-914 \
+  --trunc=1.0 \
+  --network=${NETWORK}
